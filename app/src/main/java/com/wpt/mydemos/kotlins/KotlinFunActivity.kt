fd1786f02027012cb64d3736bb761fc2abf46f53
wupingtao <wupingtao@yuxun.com.cn>
2019-09-18 11:40:34 +0800

    kotlin let with run test

--- app/src/main/java/com/wpt/mydemos/kotlins/KotlinFunActivity.kt
+++ app/src/main/java/com/wpt/mydemos/kotlins/KotlinFunActivity.kt
@@ -16,8 +16,8 @@ class KotlinFunActivity : AppCompatActivity() {
     }
 
     private fun initView(){
-        val content = letTest("wpt") + "\n" + withTest(MyBean("wpt", 22)) + "\n" +
-        runTest(MyBean("wpt", 26))
+        val content = letTest("let") + "\n" + withTest(MyBean("with", 22)) + "\n" +
+        runTest(MyBean("run", 26)) + "\n" + applyTest(MyBean("apply", 26))
         textview.text = content
     }
 
@@ -25,12 +25,13 @@ class KotlinFunActivity : AppCompatActivity() {
     /**
      * 1.可空的判断
      * 2.使用it调用对象的属性
+     * 3.let函数返回最后一行代码的值或return值
      */
     private fun letTest(str: String?): String? {
         var result = str?.let {
             Log.d("wpt_log", it)
             Log.d("wpt_log", it.length.toString())
-            "return let result"
+            "return let result=$it"
         }
         return result
     }
@@ -38,6 +39,7 @@ class KotlinFunActivity : AppCompatActivity() {
     /**
      * 1.没有可空的判断
      * 2.相对于let，不用使用it来调用
+     * 3.with函数返回最后一行代码的值或return值
      */
     private fun withTest(bean: MyBean?): String? {
         if (bean == null) {
@@ -46,7 +48,7 @@ class KotlinFunActivity : AppCompatActivity() {
         var result = with(bean) {
             Log.d("wpt_log", "name=$name")
             Log.d("wpt_log", "age=$age")
-            "return with result"
+            "return with result_name=$name ,age=$age"
         }
 
         return result
@@ -56,12 +58,27 @@ class KotlinFunActivity : AppCompatActivity() {
      * 是let于with的结合体
      * 1.去掉了let的it调用
      * 2.不用对传入的参数null判断
+     * 3.run函数返回最后一行代码的值或return值
      */
     private fun runTest(bean: MyBean?): String? {
         var result = bean?.run {
             Log.d("wpt_log", "run_name=$name")
             Log.d("wpt_log", "run_age=$age")
-            "return run result"
+            "return run result_name=$name ,age=$age"
+        }
+
+        return result
+    }
+
+    /**
+     * 1.去掉了let的it调用
+     * 2.不用对传入的参数null判断
+     * 3.run函数返回本身
+     */
+    private fun applyTest(bean: MyBean?):MyBean? {
+        var result = bean?.apply {
+            Log.d("wpt_log", "run_name=$name")
+            Log.d("wpt_log", "run_age=$age")
         }
 
         return result
@@ -71,5 +88,10 @@ class KotlinFunActivity : AppCompatActivity() {
     class MyBean(name: String, age: Int) {
         var name = name
         var age = age
+
+        override fun toString(): String {
+            return "【MyBean name=" + name + ",age=" + age + "】"
+
+        }
     }
 }
