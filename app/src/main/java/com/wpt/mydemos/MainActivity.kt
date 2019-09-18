cec18c22b9d09ffd5479c7969774d373421a6965
wupingtao <wupingtao@yuxun.com.cn>
2019-09-18 10:36:20 +0800

    kotlin run test

--- app/src/main/java/com/wpt/mydemos/MainActivity.kt
+++ app/src/main/java/com/wpt/mydemos/MainActivity.kt
@@ -10,26 +10,55 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        letTest("wpt")
-        withTest(MyBean("wpt",22))
+        Log.d("wpt_log",letTest("wpt"))
+        Log.d("wpt_log",withTest(MyBean("wpt",22)))
+        Log.d("wpt_log",runTest(MyBean("wpt",26)))
     }
 
 
-    private fun letTest(str:String?){
-        str?.let {
+    /**
+     * 1.可空的判断
+     * 2.使用it调用对象的属性
+     */
+    private fun letTest(str:String?): String? {
+        var result = str?.let {
             Log.d("wpt_log",it)
             Log.d("wpt_log", it.length.toString())
+            "return let result"
         }
+        return result
     }
 
-    private fun withTest(bean:MyBean?){
+    /**
+     * 1.没有可空的判断
+     * 2.相对于let，不用使用it来调用
+     */
+    private fun withTest(bean:MyBean?):String?{
         if (bean == null){
-            return
+            return ""
         }
-        with(bean){
+        var result = with(bean){
             Log.d("wpt_log","name=$name")
             Log.d("wpt_log","age=$age")
+            "return with result"
         }
+
+        return result
+    }
+
+    /**
+     * 是let于with的结合体
+     * 1.去掉了let的it调用
+     * 2.不用对传入的参数null判断
+     */
+    private fun runTest(bean:MyBean?):String?{
+        var result = bean?.run {
+            Log.d("wpt_log","run_name=$name")
+            Log.d("wpt_log", "run_age=$age")
+            "return run result"
+        }
+
+        return result
     }
 
 
