9b2a13697affd1dd2ff407d72fb5d2683faa9d5c
wupingtao <wupingtao@yuxun.com.cn>
2019-09-18 10:13:21 +0800

    kotlin let with test

--- app/src/main/java/com/wpt/mydemos/MainActivity.kt
+++ app/src/main/java/com/wpt/mydemos/MainActivity.kt
@@ -2,11 +2,39 @@ package com.wpt.mydemos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        letTest("wpt")
+        withTest(MyBean("wpt",22))
+    }
+
+
+    private fun letTest(str:String?){
+        str?.let {
+            Log.d("wpt_log",it)
+            Log.d("wpt_log", it.length.toString())
+        }
+    }
+
+    private fun withTest(bean:MyBean?){
+        if (bean == null){
+            return
+        }
+        with(bean){
+            Log.d("wpt_log","name=$name")
+            Log.d("wpt_log","age=$age")
+        }
+    }
+
+
+    class MyBean(name:String,age:Int){
+        var name = name
+        var age = age
     }
 }
