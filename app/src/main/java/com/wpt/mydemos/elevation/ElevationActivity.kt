cdfe7b550839b8f3c8f6c17c2ccaca33c14fdc4d
wupingtao <wupingtao@yuxun.com.cn>
2019-11-28 14:55:41 +0800

    1.阴影

--- app/src/main/java/com/wpt/mydemos/elevation/ElevationActivity.kt
+++ app/src/main/java/com/wpt/mydemos/elevation/ElevationActivity.kt
@@ -2,12 +2,20 @@ package com.wpt.mydemos.elevation
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.RelativeSizeSpan
 import com.wpt.mydemos.R
+import kotlinx.android.synthetic.main.activity_elevation.*
 
 class ElevationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_elevation)
+
+        val ssb  = SpannableString("5位")
+        ssb.setSpan(RelativeSizeSpan(0.5f),1,2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        tv_elevation.text = ssb
     }
 }
