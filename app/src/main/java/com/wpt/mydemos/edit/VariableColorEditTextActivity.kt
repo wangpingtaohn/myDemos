1233481e4553a83a9694c780057d1799ff723d9a
wupingtao <wupingtao@yuxun.com.cn>
2019-12-23 14:24:26 +0800

    layout drag

--- app/src/main/java/com/wpt/mydemos/edit/VariableColorEditTextActivity.kt
+++ app/src/main/java/com/wpt/mydemos/edit/VariableColorEditTextActivity.kt
@@ -1,9 +1,11 @@
 package com.wpt.mydemos.edit
 
 import android.os.Bundle
+import android.widget.Toast
 import com.wpt.mydemos.R
 import com.wpt.mydemos.widget.BaseActivity
 import kotlinx.android.synthetic.main.activity_variable.*
+import kotlinx.android.synthetic.main.layout_publish_vertical_item.*
 
 class VariableColorEditTextActivity : BaseActivity() {
 
@@ -24,6 +26,30 @@ class VariableColorEditTextActivity : BaseActivity() {
         }
         tv_green.setOnClickListener {
             et_content.setTextColor(resources.getColor(R.color.green))
+        }
+
+        vertical_add_pic.setOnClickListener {
+        }
+
+        vertical_add_face.setOnClickListener {
+        }
+
+        vertical_add_link.setOnClickListener {
+        }
+        vertical_add_loc.setOnClickListener {
+        }
+
+        vertical_add_at.setOnClickListener {
+        }
+
+        vertical_add_vote.setOnClickListener {
+        }
+
+
+        et_content2.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus){
+                Toast.makeText(this,"我获得焦点了",Toast.LENGTH_SHORT).show()
+            }
         }
 
     }
