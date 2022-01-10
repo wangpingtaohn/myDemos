010d3b00324565bfccbdc07594dadb20ffccb885
wangpingtao <pt_wang@juling.vip>
2022-01-10 15:03:31 +0800

    'test'

--- app/src/main/java/com/wpt/mydemos/webview/WebActivity.kt
+++ app/src/main/java/com/wpt/mydemos/webview/WebActivity.kt
@@ -1,10 +1,15 @@
 package com.wpt.mydemos.webview
 
 import android.annotation.SuppressLint
+import android.net.http.SslError
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils
-import android.webkit.WebViewClient
+import android.util.Log
+import com.tencent.smtt.export.external.interfaces.SslErrorHandler
+import com.tencent.smtt.export.external.interfaces.WebResourceError
+import com.tencent.smtt.export.external.interfaces.WebResourceRequest
+import com.tencent.smtt.sdk.WebView
+import com.tencent.smtt.sdk.WebViewClient
 import com.wpt.mydemos.R
 import kotlinx.android.synthetic.main.activity_web.*
 
@@ -14,17 +19,49 @@ class WebActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
 
-        webview.webViewClient = WebViewClient()
         val setting = webview.settings
         setting.domStorageEnabled = true
         setting.javaScriptEnabled = true
-        webview.loadUrl("file:///android_asset/jsTest.html");
+        setting.userAgentString = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:54.0) Gecko/20100101 Firefox/54.0";
+//        webview.loadUrl("file:///android_asset/jsTest.html")
+        webview.loadUrl("https://www.zhihu.com/question/509128730/answer/2292756343")
+
+        webview.webViewClient = object : WebViewClient(){
+
+            override fun onReceivedSslError(
+                p0: WebView?,
+                p1: SslErrorHandler?,
+                p2: com.tencent.smtt.export.external.interfaces.SslError?
+            ) {
+                super.onReceivedSslError(p0, p1, p2)
+                p1?.proceed()
+            }
+
+            override fun onReceivedError(
+                view: WebView?,
+                request: WebResourceRequest?,
+                error: WebResourceError?
+            ) {
+                super.onReceivedError(view, request, error)
+                Log.d("===wpt===","onReceivedError=${request?.url}")
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                Log.d("===wpt===","onPageFinished_url=${url}")
+            }
+        }
+//        webview.loadUrl("https://www.baidu.com")
 
         load_btn.setOnClickListener {
 
 
 //            webview.loadUrl("https://mtest.fantuan.cn/articleContent/fC_EajeVm")
-            webview.loadUrl("http://172.29.151.140/pinhn-apph5/appNewsDetails?articleId=6&uid=1245923&token=78574d8c43c3106124719b7b7c845569&share=2")
+            var count = 0
+            while (count < 5){
+                webview.loadUrl("https://www.bai777du.com")
+                count++
+            }
         }
 
         call_js_btn.setOnClickListener { callJs("哈哈哈","我们就是了") }
